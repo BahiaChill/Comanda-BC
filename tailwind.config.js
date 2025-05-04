ad89e746aeb95e612934ba692237e2645ac2e6ce
BahiaChill <bahiachillatencion@gmail.com>
2025-05-04 18:17:35 -0500

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-      module.exports = {
-        content: [
-          "./src/**/*.{js,jsx,ts,tsx}",
-        ],
-        theme: {
-          extend: {},
-        },
-        plugins: [],
+module.exports = {
+  content: [
+    "./index.html", // Archivo HTML principal
+    "./src/**/*.{js,jsx}" // Todos los componentes React
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3d819d", // Color principal (azul del header)
+        secondary: "#2c5d73" // Color secundario (hover de botones)
+      },
+      container: {
+        center: true, // Centra el contenedor
+        padding: "1rem" // Padding lateral en móviles
       }
+    }
+  },
+  plugins: [],
+}
